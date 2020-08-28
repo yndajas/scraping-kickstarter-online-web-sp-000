e82783efcbd34e833ea8d06b55501ef418a32b82
Ynda Jas <yndajas@gmail.com>
2020-08-28 16:59:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
+  # projects: kickstarter.css("li.project.grid_4")
 end
 
 create_project_hash
