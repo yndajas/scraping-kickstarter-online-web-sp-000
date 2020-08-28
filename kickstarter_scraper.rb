362ee6ff0daa6ff98bd7988aafc5a3b1e0151324
Ynda Jas <yndajas@gmail.com>
2020-08-28 17:16:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
+  # location: project.css("span.location-name").text
 end
 
 create_project_hash
