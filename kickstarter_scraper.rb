6ab0687a4bfa4fdeb7cd2eab4717a8dc821689a4
Ynda Jas <yndajas@gmail.com>
2020-08-28 17:04:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
   # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
 end
 
 create_project_hash
