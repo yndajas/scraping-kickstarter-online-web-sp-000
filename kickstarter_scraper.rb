c25db8d9ac180d0825c94ca0f18065cf7fdfc143
Ynda Jas <yndajas@gmail.com>
2020-08-28 17:30:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,12 +10,17 @@ def create_project_hash
   
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text
+      :location => project.css("span.location-name").text,
+      
+    }
   end
   
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css("span.location-name").text
+  # image link: 
+  # description: 
+  # location: 
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   projects
 end
